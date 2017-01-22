45dc6440e6e228b2ed6ce702e03cefa5f0d15cec
Arnav Mittal <mittal13@purdue.edu>
2017-01-22 10:31:41 -0500

    Added final files after final change

--- mlModule.py
+++ mlModule.py
@@ -1,25 +1,35 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 import Leap, sys
+import os
 
 class SampleListener(Leap.Listener):
-    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-    angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0}
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky', 'TI', 'IM', 'MR', 'RP']
+    angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0, 'TI': 0, 'IM': 0, 'MR': 0, 'RP': 0}
     def on_frame(self, controller):
         pass
     def reset_values(self):
-        self.angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0}
+        self.angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0, 'TI': 0, 'IM': 0, 'MR': 0, 'RP': 0}
 
 def analyzeFrame( listenerObj, currentFrame, letter ):
     frame = currentFrame
     listenerObj.reset_values()
     for hand in frame.hands:
         palmNormal = hand.palm_normal
-
+        prevFingerDirection = Leap.Vector(0, 0, 0)
         #   Get fingers
         for finger in hand.fingers:
             fingerAngle = finger.direction.angle_to(palmNormal) * 57.2958
             listenerObj.angles[listenerObj.finger_names[finger.type]] = fingerAngle
+            if (finger.type == 1):
+                listenerObj.angles['TI'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            if (finger.type == 2):
+                listenerObj.angles['IM'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            if (finger.type == 3):
+                listenerObj.angles['MR'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            if (finger.type == 4):
+                listenerObj.angles['RP'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            prevFingerDirection = finger.direction
 
     #   Print avg values
     #print (listenerObj.angles)
@@ -28,8 +38,8 @@ def analyzeFrame( listenerObj, currentFrame, letter ):
 def main():
     # read training set
     print "Reading training set... "
-    features = np.loadtxt('training_set.txt', usecols=(0, 1, 2, 3, 4))
-    values = np.loadtxt('training_set.txt', dtype='c', usecols=(5), unpack = True)
+    features = np.loadtxt('training_set.txt', usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8), delimiter='\t')
+    values = np.loadtxt('training_set.txt', dtype='c', usecols=(9), unpack = True, delimiter='\t')
 
     # create classifier
     clf = GaussianNB()
@@ -51,7 +61,13 @@ def main():
             currentFrame = controller.frame()
             angles = analyzeFrame(listener, currentFrame, tc)
             print angles
-            print(clf.predict([[angles['Thumb'],angles['Index'], angles['Middle'], angles['Ring'], angles['Pinky']]]))
+            p=(clf.predict([[angles['Thumb'],angles['Index'], angles['Middle'], angles['Ring'], angles['Pinky'], angles['TI'], angles['IM'],angles['MR'], angles['RP']]]))
+            if( p == "1" ):
+                print( "I love Purdue" )
+                os.system("say I love Purdue")
+            else:
+                print (p)
+                os.system("say %s"%(p[0]))
     except KeyboardInterrupt:
         pass
     finally:
--- train.py
+++ train.py
@@ -10,41 +10,61 @@ import Leap, sys
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-    angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0}
+    angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0, 'TI': 0, 'IM': 0, 'MR': 0, 'RP': 0}
     def on_frame(self, controller):
         frame = controller.frame()
         self.reset_values()
         for hand in frame.hands:
             palmNormal = hand.palm_normal
-
+            prevFingerDirection = Leap.Vector(0,0,0)
             #   Get fingers
             for finger in hand.fingers:
                 fingerAngle = finger.direction.angle_to(palmNormal) * 57.2958
                 self.angles[self.finger_names[finger.type]] = fingerAngle
+                if(finger.type == 1):
+                    self.angles['TI'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+                if (finger.type == 2):
+                    self.angles['IM'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+                if (finger.type == 3):
+                    self.angles['MR'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+                if (finger.type == 4):
+                    self.angles['RP'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+                prevFingerDirection = finger.direction
 
         # Print avg values
         # print (listenerObj.angles)
         print self.angles
 
     def reset_values(self):
-        self.angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0}
+        self.angles = {'Thumb': 0, 'Index': 0, 'Middle': 0, 'Ring': 0, 'Pinky': 0, 'TI': 0, 'IM': 0, 'MR': 0, 'RP': 0}
 
 def analyzeFrame( listenerObj, currentFrame, letter ):
     frame = currentFrame
     listenerObj.reset_values()
+
     for hand in frame.hands:
         palmNormal = hand.palm_normal
+        prevFingerDirection = Leap.Vector(0, 0, 0)
 
         #   Get fingers
         for finger in hand.fingers:
             fingerAngle = finger.direction.angle_to(palmNormal) * 57.2958
             listenerObj.angles[listenerObj.finger_names[finger.type]] = fingerAngle
+            if (finger.type == 1):
+                listenerObj.angles['TI'] = finger.direction.angle_to(prevFingerDirection)  * 57.2958
+            if (finger.type == 2):
+                listenerObj.angles['IM'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            if (finger.type == 3):
+                listenerObj.angles['MR'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            if (finger.type == 4):
+                listenerObj.angles['RP'] = finger.direction.angle_to(prevFingerDirection) * 57.2958
+            prevFingerDirection = finger.direction
 
     #   Print avg values
     if len(frame.hands) != 0:
         print (listenerObj.angles)
         with open("training_set.txt", 'a') as feature_file:
-            feature_file.write("%f\t%f\t%f\t%f\t%f\t%s" % (listenerObj.angles['Thumb'], listenerObj.angles['Index'], listenerObj.angles['Middle'], listenerObj.angles['Ring'], listenerObj.angles['Pinky'], letter))
+            feature_file.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%s" % (listenerObj.angles['Thumb'], listenerObj.angles['Index'], listenerObj.angles['Middle'], listenerObj.angles['Ring'], listenerObj.angles['Pinky'], listenerObj.angles['TI'], listenerObj.angles['IM'], listenerObj.angles['MR'], listenerObj.angles['RP'], letter))
 
 
 def main():
